703ff31b12aec0af92c29bd2b3a6436cac188c0b
KatoMasami <163547645+katomasami@users.noreply.github.com>
2024-06-06 19:18:23 +0000

    [Finish] 3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,22 @@
 class ListsController < ApplicationController
   def new
      # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @list = List.new  
+    @list = List.new
   end
 
   def create
-    
-    list = List.new(list_params)
-    
+
+    @list = List.new(list_params)
+
     if @list.save
-      redirect_to list_path(list.id)
+      redirect_to list_path(@list.id)
     else
       render :new
     end
-    
+
   end
-  
-  
+
+
   def index
      @lists = List.all
   end
@@ -28,26 +28,26 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
-  
+
+
   def destroy
-    list = List.find(params[:id]) 
+    list = List.find(params[:id])
     list.destroy
-    redirect_to '/lists'  
+    redirect_to '/lists'
   end
-  
-  
+
+
   private
-  
-  
+
+
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
-  
+
 end
